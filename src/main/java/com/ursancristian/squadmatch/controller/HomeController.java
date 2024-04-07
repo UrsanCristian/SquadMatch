5c50611b3be09c0abb12344d8142f5fa36240004
Ursan Cristian <cristian.ursan99@gmail.com>
2024-04-07 20:25:13 +0300

    Naming fixes

--- src/main/java/com/ursancristian/squadmatch/controller/HomeController.java
+++ src/main/java/com/ursancristian/squadmatch/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home(Model model) {
+    public String homePage(Model model) {
         model.addAttribute("title", "Squad Match");
         return "home";
     }
