dfdd3b21bf1a85835c45863d9105e617732cb972
Ursan Cristian <cristian.ursan99@gmail.com>
2024-04-07 20:24:56 +0300

    Created LobbyRepository

---
+++ src/main/java/com/ursancristian/squadmatch/repository/LobbyRepository.java
@@ -0,0 +1,8 @@
+package com.ursancristian.squadmatch.repository;
+
+import com.ursancristian.squadmatch.model.Lobby;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LobbyRepository extends JpaRepository<Lobby, Integer> {
+
+}
