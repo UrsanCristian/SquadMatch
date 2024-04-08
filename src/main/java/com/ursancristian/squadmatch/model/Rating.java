fe741e1544805277107750c8829a742bb6f0a46f
Ursan Cristian <cristian.ursan99@gmail.com>
2024-04-08 20:48:42 +0300

    Annotations fixes

--- src/main/java/com/ursancristian/squadmatch/model/Rating.java
+++ src/main/java/com/ursancristian/squadmatch/model/Rating.java
@@ -1,22 +1,38 @@
 package com.ursancristian.squadmatch.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class Rating {
     @Id
     @GeneratedValue
     private int id;
 
-    private String playerFrom;
+    @ManyToOne
+    @JoinColumn(name = "player_from_id")
+    private User playerFrom;
 
-    private String playerTo;
+    @ManyToOne
+    @JoinColumn(name = "player_to_id")
+    private User playerTo;
+
     @ManyToOne
     private Lobby lobby;
 
+    @Min(1)
+    @Max(10)
     private int behavior;
 
+    @Min(1)
+    @Max(10)
     private int skills;
 }
