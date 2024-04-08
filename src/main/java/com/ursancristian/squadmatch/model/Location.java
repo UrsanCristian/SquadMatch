8cf90b348542f4ee1d1bfc8b0b12ed60fc554614
Ursan Cristian <cristian.ursan99@gmail.com>
2024-04-08 20:48:39 +0300

    Annotations fixes

--- src/main/java/com/ursancristian/squadmatch/model/Location.java
+++ src/main/java/com/ursancristian/squadmatch/model/Location.java
@@ -4,12 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class Location {
     @Id
     @GeneratedValue
