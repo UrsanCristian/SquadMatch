bac7ae2dd80518ce746b6c15643415403fae945b
Ursan Cristian <cristian.ursan99@gmail.com>
2024-04-19 15:41:10 +0300

    Created Mappings for Lobby creation functionality

--- src/main/java/com/ursancristian/squadmatch/controller/LobbyController.java
+++ src/main/java/com/ursancristian/squadmatch/controller/LobbyController.java
@@ -1,12 +1,18 @@
 package com.ursancristian.squadmatch.controller;
 
+import com.ursancristian.squadmatch.dto.LobbyDTO;
 import com.ursancristian.squadmatch.model.Lobby;
+import com.ursancristian.squadmatch.model.Location;
 import com.ursancristian.squadmatch.repository.LobbyRepository;
+import com.ursancristian.squadmatch.repository.LocationRepository;
+import com.ursancristian.squadmatch.service.LobbyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -15,6 +21,8 @@ import java.util.List;
 public class LobbyController {
 
     private final LobbyRepository lobbyRepository;
+    private final LobbyService lobbyService;
+    private final LocationRepository locationRepository;
 
     @GetMapping("/matches")
     public String lobbyList(Model model) {
@@ -31,4 +39,22 @@ public class LobbyController {
         model.addAttribute("lobby", lobby);
         return "lobby_details";
     }
+
+    @GetMapping("/matches/create-lobby")
+    public String createLobbyPage(Model model) {
+        List<Location> locations = locationRepository.findAll();
+
+        model.addAttribute("title", "Create Lobby");
+        model.addAttribute("lobby", new LobbyDTO());
+        model.addAttribute("locations", locations);
+
+        return "lobby_create";
+    }
+
+    @PostMapping("/matches/create-lobby")
+    public String createLobbySubmit(@ModelAttribute LobbyDTO lobbyDto) {
+        Lobby lobby = lobbyService.createLobby(lobbyDto);
+        return "redirect:/matches";
+    }
+
 }
